728f26173d12cf652461c1d8458a0f1c4a8b7884
Juan <juan.antonadelval@gmail.com>
2015-04-06 19:43:48 +0200

    add_post_method

--- BasicBlog.rb
+++ BasicBlog.rb
@@ -1,7 +1,55 @@
+############################
+#CLASS DECALARTION
+############################
+
 class Blog
+
+  def initialize
+  	@post=[]
+  end	
+
+  def add_post(title)
+    @post=@post
+  end	
+
+  def create_front_page
+
+  end
+
+  def publish_front_page
+  end
+
+class Post
+
+  attr_accessor :title
+  attr_accessor :test
+  attr_accessor :date
+
+  def initialize(title, date, test)
+    @title
+    @date
+    @test
+  end
+
+  def title
+    @title	
+  end
+
+  def date
+    @date	
+  end
+
+  def test
+    @test	
+  end
 
 end
 
-class post
+post1 = Post.new "Title_1", Time.now, "Text_1"
+post2 = Post.new "Title_2", Time.now, "Text_2"
+post3 = Post.new "Title_3", Time.now, "Text_3"
 
-end 
+blog.add_post 
+
+
+	
